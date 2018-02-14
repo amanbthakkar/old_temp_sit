89ee98befbeabae34591514e9c4e179631a7728d
disha09drs <36405427+disha09drs@users.noreply.github.com>
2018-02-14 17:00:46 +0530

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,57 +1,64 @@
-<?php
-//session_start();
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "galaxyhotel";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-  
-$sql="SELECT * FROM customer";
-$query_run=mysqli_query($conn,$sql);
-$number=mysqli_num_rows($query_run);
-  
-$sql="INSERT INTO customer VALUES ($number+1,'$_POST[name]','$_POST[sname]','$_POST[mno]','$_POST[email]','$_POST[address]','$_POST[checkin]','$_POST[checkout]','$_POST[nop]','$_POST[room]')";
-$query_run=mysqli_query($conn,$sql);
-?>
-
 <html>
-<head><h1>Galaxy Hotel</h1>
-<link rel="stylesheet" type="text/css" href="Header.css" title="Header">
-<script type ='text/javascript' src='script.js' ></script>     
+<head><h1> Galaxy hotel </h1>
+<link rel = "stylesheet" type="text/css" href="Header.css" title ="Header">
 </head>
-<body onload='document.form1.text1.focus()'> 
-<body class="bg">
-
-<?php
-
-session_start();
-
-//Set session variables
-$_SESSION["name"] = $_POST["name"];
-
-echo "Hi " . $_SESSION["name"] . ", your stay details have been recorded. Please proceed to pay!";
-
-?>
-
-<div class ="form1">
-<form name="form1" action="insertp.php" method="post"> 
-CREDIT/DEBIT<br> <br>
-Name<input type ="text" name="pname">
-<BR>
-<BR>
-Card-Number<input type ="number" name ="cno">
-<br>
-<br>
-CVV <input type ="text" name="cvv">
-<br><br>
-Expiry <input type="date" name="exp"><br><br>
-<center><input type ="submit" value ="SUBMIT" onclick="ValidateName(document.form1.text1);"  style="background-color: #191970;
-    border: none;
-	font-family: Georgia;
-	font-size: 30px; color:white"> </center>
-</form>
+<body class ="bg">
+	<p><?php echo "Thank You '$_POST[name]'!" ?></p>
+<div class = "tabs">
+<a href= "Header.html" class="tab" >Home</a>
+<a href= "Rooms.html" class="tab" >Rooms</a>
+<a href= "Dining.html" class="tab" >Dining</a>
+<a href= "Amenities.html" class="tab" >Amenities</a>
+<a href= "form2.html" class="tab" >Book now </a>
+<a href= "form1.html" class="tab" > Login </a> 
 </div>
+<br>
+<div class = "container">
+	<div class ="slide" >
+	<img src = "img1.jpg" class="image">
+	</div>
+	
+	<div class="slide">
+	<img src ="img2.jpg" class="image">
+	</div>
+	
+	<div class ="slide">
+	<img src ="img3.jpg" class="image">
+	</div>
+	
+	<button class="prev" onclick="plusSlides(-1)">&#10094;</button>
+	<button class="next" onclick="plusSlides(+1)">&#10095;</button>
+
+</div>
+<script>
+var slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  <!--var dots = document.getElementsByClassName("dot");--!>
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  <!--for (i = 0; i < dots.length; i++) {--!>
+      <!--dots[i].className = dots[i].className.replace(" active", "");--!>
+  <!--}--!>
+  slides[slideIndex-1].style.display = "block"; 
+  <!--dots[slideIndex-1].className += " active";--!>
+}
+</script>
+<br>
+<br>
+<br>
 
 </body>
+
 </html>
